7fb5e63d73f01a33a7b7d45bb651e1ca5440862b
v02qabs <takesue090@gmail.com>
2023-11-27 16:57:27 +0900

    curses-getstr-loop-while-fopen-fclose

--- curses/getstr.py
+++ curses/getstr.py
@@ -1,7 +1,15 @@
 import curses
 
-stdscr = curses.initscr()
+def curses_main(args):
+    w = curses.initscr()
 
-stdscr.clear()
-strs = stdscr.getstr()
-
+    f = open('myfile.txt', 'w')
+    curses.echo()
+    while 1:
+        w.addstr(0,0,">")
+        w.clrtoeol()
+        s = w.getstr()
+        f.write(s)
+        if s=="q":
+            break
+curses.wrapper(curses_main)
